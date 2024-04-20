8d35f61694a49fbee5df7d358f38d8669b05e550
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-04-20 11:58:47 +0530

    modified app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+// App.jsx
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Navbar from './components/Navbar'; // Import Navbar
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Footer from './components/Footer'; // Import Footer
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <Navbar /> {/* Render Navbar */}
+        <Home />
+        <Footer /> {/* Render Footer */}
+      </div>
+    ),
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/Login', // Capitalize "L" in "Login"
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  ); 
 }
 
-export default App
+export default App;
