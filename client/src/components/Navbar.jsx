77331215c3177dfa181d439f3d0b100bf0e12729
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-06 21:38:24 +0530

    chore: Update Navbar component with additional links and styles

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,31 @@ function Navbar() {
           <img src={Logo} alt="" />
         </div>
         <div className="links">
-          <Link to="/">ART</Link>
+          <Link className='link' to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to="/?cat=SCIENCE">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to="/?cat=TECHNOLOGY">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to="/?cat=CINEMA">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to="/?cat=DESIGN">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to="/?cat=FOOD">
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link to="/write">Write</Link>
+          </span>
+
+
         </div>
 
       </div>
