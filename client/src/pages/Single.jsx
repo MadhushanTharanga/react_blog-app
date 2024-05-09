92766f91be4298fea94b16f8e4aaea105d320c5f
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-09 12:13:11 +0530

    Update Single page with lazy loading for images

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Single() {
   return (
-    <div>Single</div>
+    <div className="single">
+      <div className="content">
+        <img src="https://images.pexels.com/photos/19896879/pexels-photo-19896879/free-photo-of-harbor-in-stockholm-sweden.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/21733725/pexels-photo-21733725/free-photo-of-a-woman-taking-a-picture-of-the-water-from-a-boat.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <div className="info">
+          <span> John</span>
+          <p>Posted 2days ago</p>
+        </div>
+        </div>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
