f6b843ba016682eeb9a5597af8c0be5076c7f318
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-10 20:29:51 +0530

    feat: Add react-quill package for rich text editor functionality

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function Write() {
   return (
-    <div>Write</div>
+    <div className='add' >
+      <div className="content">
+        <input type="text" placeholder='Title' className="text" />
+        <div className="editorContainer"></div>
+      </div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
+    </div>
   )
 }
 
