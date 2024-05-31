36706345c575a76af96d1c7c06af34fa08714c03
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-31 20:56:21 +0530

    Add API endpoint for testing and return "It works!"

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/test",(req,res)=>{
-    res.json("It works!")
-})
+app.use("/api/posts", postRoutes);
 
 
 app.listen(8800,()=>{
